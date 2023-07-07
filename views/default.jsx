920663472f7be520d9ba271c861d0e3f0c54ac6a
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-07-07 17:18:45 -0400

    adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,15 @@
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
+
+  
+module.exports = Def
