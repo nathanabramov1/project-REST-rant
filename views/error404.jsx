5f8473301e0bd32fab84218397c025a0fc3712f5
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-07-09 14:23:52 -0400

    still have to make the table

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,13 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img src="/images/error-cat.jpg" alt="cat-errorPic" />
+              </div>
+              <div>
+              Photo by <a href="https://unsplash.com/@madhatterzone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Manja Vitolic</a> on <a href="https://unsplash.com/images/animals/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+              </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/burger.jpg" alt="burger"  />
+              </div>
+              <div>
+              Photo by <a href="https://unsplash.com/@1ncreased?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lidye</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+              </div>
               <a href="/places">
                <button className="btn-primary">Places Page</button>
               </a>
